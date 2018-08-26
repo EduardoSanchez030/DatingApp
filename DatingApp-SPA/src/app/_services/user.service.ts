b3e0283f25336f499789f9c72a8ce4b2500e8e79
unknown <eduardosanchez030@gmail.com>
2018-08-25 19:15:16 -0500

    End of Section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+baseUrl = environment.apiUrl;
+
+
+constructor(private http: HttpClient) { }
+
+getUsers(): Observable<User[]> {
+ return this.http.get<User[]>(this.baseUrl + 'Users');
+}
+
+getUser(id: number): Observable<User>  {
+  return this.http.get<User>(this.baseUrl + 'Users/' + id);
+}
+
+}
